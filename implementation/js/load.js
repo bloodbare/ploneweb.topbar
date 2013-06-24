3ead703c752db6a094a434a06cedf32c16c8370b
David Batranu <dbatranu@gmail.com>
2013-06-24 14:24:05 +0300

    Starting mockup implementation
    - copying font and image folders
    - creating js and css folders
    - splitting html and css
    - starting work on jsonp based JS widget (load.js)
    - starting work on widget css reset

---
+++ implementation/js/load.js
@@ -0,0 +1,34 @@
+(function(){
+  var target = jQuery('#topbarcontainer');
+  var baseURL = 'http://static.david.edw.ro/ploneweb.topbar/implementation';
+  function loadWidgetHTML() {
+    jQuery.ajax({
+      url: baseURL + "/js/index.js",
+      jsonpCallback: "populateWidget",
+      jsonp: false,
+      dataType: "jsonp",
+      success: function(data){
+        console.log(data);
+        var dataHTML = data.join('\n');
+        var html = dataHTML.replace(/\{\{ BASE_URL \}\}/g, baseURL);
+        target.html(html);
+      }
+    });
+  }
+  function loadWidgetCSS() {
+    var cssPaths = ['/fonts/stylesheet.css', '/css/topbar.css'];
+    jQuery.each(cssPaths, function(i, cssPath){
+      var cssLink = jQuery('<link>', {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: baseURL + cssPath
+      });
+      cssLink.appendTo('head');
+    });
+  }
+  jQuery(document).ready(function(){
+    console.log('READY!');
+    loadWidgetHTML(target);
+    loadWidgetCSS(target);
+  });
+})();
